573eb6e14b2d9c7c725a6861141183d8f8805912
AAPho <austinanpho@gmail.com>
2023-12-07 20:18:34 -0700

    initial

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -6,14 +6,57 @@ public class SortingUtility {
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
 
         // TODO implement Gnome Sort here
+
+        int pos = 0;
+
+        while (pos < data.length) {
+            if (pos == 0 || data[pos].compareTo(data[pos - 1]) >= 0) {
+                pos++;
+            } else {
+
+                T temp = data[pos];
+                data[pos] = data[pos - 1];
+                data[pos - 1] = temp;
+
+                pos--;
+            }
+        }
+
     }
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
 
         // TODO implement Cocktail Shaker Sort here
-    }
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
 
+                    T temp = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+
+            swapped = false;
+            for (int i = data.length - 1; i > 0; i--) {
+                if (data[i].compareTo(data[i - 1]) < 0) {
+
+                    T temp = data[i];
+                    data[i] = data[i - 1];
+                    data[i - 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
 
     public static <T extends Comparable<T>> void shellSort(T[] data) {
 
